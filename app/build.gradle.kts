67deb8290dea6b5bd6eb1ca437edc426f392c744
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-05 12:24:52 +0200

    test: add JVM unit test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,5 +62,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-ads:25.4.0")
     implementation("com.google.android.ump:user-messaging-platform:4.0.0")
 
+    testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
