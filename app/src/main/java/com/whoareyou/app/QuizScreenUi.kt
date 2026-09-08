4b67f802d6772c8c4af38b2b7c69e75aeaa95ae6
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-08 17:17:21 +0200

    M48 clean quiz polish imports

--- app/src/main/java/com/whoareyou/app/QuizScreenUi.kt
+++ app/src/main/java/com/whoareyou/app/QuizScreenUi.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun QuizScreen(
