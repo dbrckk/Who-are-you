1ae8d1b189daa105f48a9ef51377b24c40f16c1f
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-09 21:18:13 +0200

    M60 fix Compose v2 test imports

--- app/src/androidTest/java/com/whoareyou/app/MainFlowE2eTest.kt
+++ app/src/androidTest/java/com/whoareyou/app/MainFlowE2eTest.kt
@@ -3,10 +3,8 @@ package com.whoareyou.app
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.test.SemanticsMatcher
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodes
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
