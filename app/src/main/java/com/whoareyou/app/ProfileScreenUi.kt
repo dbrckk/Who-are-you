aa5a5b0ef7ce1d81fa925407052d6a44c4113b17
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 13:24:52 +0200

    M299 center profile content on large screens

--- app/src/main/java/com/whoareyou/app/ProfileScreenUi.kt
+++ app/src/main/java/com/whoareyou/app/ProfileScreenUi.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -64,7 +65,8 @@ fun ProfileScreen(
 
     LazyColumn(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxHeight()
+            .readableContentWidth()
             .background(
                 Brush.verticalGradient(
                     listOf(
