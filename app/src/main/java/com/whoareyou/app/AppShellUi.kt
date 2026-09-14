056e2613e3c4813e59d1e6f2c0d005cf122911b9
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 18:09:54 +0200

    M584 remove invalid Compose weight import

--- app/src/main/java/com/whoareyou/app/AppShellUi.kt
+++ app/src/main/java/com/whoareyou/app/AppShellUi.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
