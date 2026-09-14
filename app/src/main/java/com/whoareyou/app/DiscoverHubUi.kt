89e92730d577ec1265f1dd48593da88b3405112d
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 13:58:11 +0200

    M378 expose Discover list for frame benchmarks

--- app/src/main/java/com/whoareyou/app/DiscoverHubUi.kt
+++ app/src/main/java/com/whoareyou/app/DiscoverHubUi.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -72,6 +73,7 @@ fun DiscoverHub(
     LazyColumn(
         modifier = Modifier
             .fillMaxHeight()
+            .testTag("discover_list")
             .readableContentWidth()
             .background(
                 Brush.verticalGradient(
