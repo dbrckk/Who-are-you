666fb07bf48f24d71c7fab893d4d8cc10f0c3f7a
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 13:24:47 +0200

    M297 center quiz content on large screens

--- app/src/main/java/com/whoareyou/app/QuizScreenUi.kt
+++ app/src/main/java/com/whoareyou/app/QuizScreenUi.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -56,7 +57,8 @@ fun QuizScreen(
 
     Column(
         Modifier
-            .fillMaxSize()
+            .fillMaxHeight()
+            .readableContentWidth()
             .testTag("quiz_question_${safeQuestionIndex + 1}")
             .background(
                 Brush.verticalGradient(
