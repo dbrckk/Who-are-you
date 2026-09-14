5e66082af64d44e5b0c2d0159d5e719980994e47
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 12:53:01 +0200

    M213 fix privacy support UI import

--- app/src/main/java/com/whoareyou/app/DiscoverHubUi.kt
+++ app/src/main/java/com/whoareyou/app/DiscoverHubUi.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
