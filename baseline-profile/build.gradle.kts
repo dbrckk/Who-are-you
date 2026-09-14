b72540648fe9fd678d021a49b7e1605678d4c016
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 13:52:21 +0200

    M373 add stable UI Automator to performance module

--- baseline-profile/build.gradle.kts
+++ baseline-profile/build.gradle.kts
@@ -30,4 +30,5 @@ dependencies {
     implementation("androidx.test.ext:junit:1.3.0")
     implementation("androidx.test:runner:1.7.0")
     implementation("androidx.benchmark:benchmark-macro-junit4:1.5.0")
+    implementation("androidx.test.uiautomator:uiautomator:2.4.0")
 }
