0f6876acb21199d425f446ebb3178a077aff5c10
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 13:49:27 +0200

    M360 add Baseline Profile build plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
     id("com.android.application") version "9.3.1" apply false
+    id("com.android.test") version "9.3.1" apply false
+    id("androidx.baselineprofile") version "1.5.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
