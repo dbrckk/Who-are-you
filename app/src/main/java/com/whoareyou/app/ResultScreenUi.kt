b8554cee1da90aabdc8ed7f5eb417368f7ef2d80
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 13:24:49 +0200

    M298 center result content on large screens

--- app/src/main/java/com/whoareyou/app/ResultScreenUi.kt
+++ app/src/main/java/com/whoareyou/app/ResultScreenUi.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -73,7 +74,8 @@ fun ResultScreen(
 
     LazyColumn(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxHeight()
+            .readableContentWidth()
             .background(
                 Brush.verticalGradient(
                     listOf(
