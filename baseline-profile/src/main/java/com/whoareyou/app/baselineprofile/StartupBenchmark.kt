9dba512a284e1afc8b8d83e4d54506692f94f1ba
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 13:50:20 +0200

    M367 measure cold and warm startup with Macrobenchmark

---
+++ baseline-profile/src/main/java/com/whoareyou/app/baselineprofile/StartupBenchmark.kt
@@ -0,0 +1,43 @@
+package com.whoareyou.app.baselineprofile
+
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class StartupBenchmark {
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun coldStartupWithBaselineProfile() = startup(StartupMode.COLD)
+
+    @Test
+    fun warmStartupWithBaselineProfile() = startup(StartupMode.WARM)
+
+    private fun startup(mode: StartupMode) = benchmarkRule.measureRepeated(
+        packageName = PACKAGE_NAME,
+        metrics = listOf(StartupTimingMetric()),
+        compilationMode = CompilationMode.Partial(
+            baselineProfileMode = CompilationMode.Partial.BaselineProfileMode.Require
+        ),
+        startupMode = mode,
+        iterations = 10,
+        setupBlock = {
+            pressHome()
+        }
+    ) {
+        startActivityAndWait()
+    }
+
+    private companion object {
+        const val PACKAGE_NAME = "com.whoareyou.app"
+    }
+}
