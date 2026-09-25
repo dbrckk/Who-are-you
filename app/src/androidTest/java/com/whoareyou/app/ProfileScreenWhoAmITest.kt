347956e392fde7d927fdb12e3e77be605aaa8e7c
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-25 20:45:29 +0200

    test: scroll profile habits entry into view

--- app/src/androidTest/java/com/whoareyou/app/ProfileScreenWhoAmITest.kt
+++ app/src/androidTest/java/com/whoareyou/app/ProfileScreenWhoAmITest.kt
@@ -3,6 +3,7 @@ package com.whoareyou.app
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performScrollTo
 import org.junit.Rule
 import org.junit.Test
 
@@ -52,7 +53,7 @@ class ProfileScreenWhoAmITest {
         }
 
         composeRule.onNodeWithTag("who_am_i_portrait").assertIsDisplayed()
-        composeRule.onNodeWithTag("profile_open_habits").assertIsDisplayed()
+        composeRule.onNodeWithTag("profile_open_habits").performScrollTo().assertIsDisplayed()
         composeRule.onNodeWithTag("profile_habits_privacy_hint").assertIsDisplayed()
     }
 }
